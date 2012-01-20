2a70156a55740319597b03e9fabae3d5e7de00a8
Aaron Steele <eightysteele@gmail.com>
2012-01-19 21:22:58 -0800

    Cleanup.

--- app-cartodb/js/mol.map.tiles.js
+++ app-cartodb/js/mol.map.tiles.js
@@ -149,36 +149,9 @@ mol.modules.map.tiles = function(mol) {
                 z = z - 1;
                 /* Create a statement for all points and a style for those points at zoom>=10 */
                 sql = "SELECT 1 as cnt, the_geom_webmercator FROM eafr WHERE scientificname = '" + this.name + "'";
-                this.style = "%23eafr { marker-fill:%23E25B5B; marker-opacity:0.9; marker-width:3; marker-line-color:white; marker-line-width:1; marker-line-opacity:0.8; marker-placement:point;	marker-type:ellipse; marker-allow-overlap:true; } ";
+                this.style = "%23eafr { marker-fill:%23E25B5B; marker-opacity:0.9; marker-width:5; marker-line-color:white; marker-line-width:1; marker-line-opacity:0.8; marker-placement:point;	marker-type:ellipse; marker-allow-overlap:true; } ";
                 this.statements[this.topZ] = "sql=" + sql + "&style=" + this.style;
             }
         }
-        //     createCalls: function(seed) {
-        //         var z = this.bottomZ;
-        //         this.topZ = this.bottomZ + 6;
-        //         /* Define our base grid style */
-        //         var baseStyle = "%23eafr{ polygon-fill:%23EFF3FF; polygon-opacity:0.6; line-opacity:1; line-color:%23FFFFFF;  ";
-        //         /* Add grid colors based on count (cnt) in clusters */
-        //         baseStyle += "[cnt>160]{ polygon-fill:%2308519C; polygon-opacity:0.7;} [cnt<160]{polygon-fill:%233182BD; polygon-opacity:0.65;} [cnt<80]{ polygon-fill:%236BAED6; polygon-opacity:0.6;} [cnt<40]{ polygon-fill:%239ECAE1; polygon-opacity:0.6;} [cnt<20]{polygon-fill:%23C6DBEF; polygon-opacity:0.6;} [cnt<2]{ polygon-fill:%23EFF3FF; polygon-opacity:0.4; }} ";
-                
-        //         /* Get the first level (lowest zoom) set of points clustered */
-        //         var sql = "SELECT cnt, ST_Transform(ST_Envelope(GEOMETRYFROMTEXT('LINESTRING('||(st_xmax(the_geom)-" + (seed / 2) + ")||' '||(st_ymax(the_geom)-" + (seed / 2) + ")||', '||(st_xmax(the_geom)%2B" + (seed / 2) + ")||' '||(st_ymax(the_geom)%2B" + (seed / 2) + ")||')',4326)),3857) as the_geom_webmercator FROM (SELECT count(*) as cnt, scientificname, ST_SnapToGrid(the_geom, 0%2B" + (seed / 2) + ", 75%2B" + (seed / 2) + ", " + seed + ", " + seed + ") as the_geom FROM eafr GROUP By eafr.scientificname, ST_SnapToGrid(the_geom, 0%2B" + (seed / 2) + ", 75%2B" + (seed / 2) + ", " + seed + ", " + seed + ")) eafr WHERE eafr.scientificname='" + this.name + "' AND ST_Intersects(the_geom, GEOMETRYFROMTEXT('MULTIPOLYGON(((-180 75, -180 -75, 180 -75, 180 75, -180 75)))',4326))";
-        //         seed = seed / 2;
-        //         this.style = baseStyle;
-        //         this.statements[0] = "sql=" + sql + "&style=" + this.style;
-        //         /* Create a clustering SQL statement and a style for each zoom < topZ */
-        //         while (z < this.topZ) {
-        //             sql = "SELECT cnt, scientificname, ST_Transform(ST_Envelope(GEOMETRYFROMTEXT('LINESTRING('||(st_xmax(the_geom)-" + (seed / 2) + ")||' '||(st_ymax(the_geom)-" + (seed / 2) + ")||', '||(st_xmax(the_geom)%2B" + (seed / 2) + ")||' '||(st_ymax(the_geom)%2B" + (seed / 2) + ")||')',4326)),3857) as the_geom_webmercator FROM (SELECT count(*) as cnt, scientificname, ST_SnapToGrid(the_geom, 0%2B" + (seed / 2) + ", 75%2B" + (seed / 2) + ", " + seed + ", " + seed + ") as the_geom FROM eafr GROUP By eafr.scientificname, ST_SnapToGrid(the_geom, 0%2B" + (seed / 2) + ", 75%2B" + (seed / 2) + ", " + seed + ", " + seed + ")) eafr WHERE eafr.scientificname='" + this.name + "' AND ST_Intersects(the_geom, GEOMETRYFROMTEXT('MULTIPOLYGON(((-180 75, -180 -75, 180 -75, 180 75, -180 75)))',4326))";
-        //             this.statements[z] = "sql=" + sql + "&style=" + this.style;
-        //             z++;
-        //             seed = seed / 2;
-        //         }
-        //         z = z - 1;
-        //         /* Create a statement for all points and a style for those points at zoom>=10 */
-        //         sql = "SELECT 1 as cnt, the_geom_webmercator FROM eafr WHERE scientificname = '" + this.name + "'";
-        //         this.style = "%23eafr { marker-fill:%23E25B5B; marker-opacity:0.9; marker-width:3; marker-line-color:white; marker-line-width:1; marker-line-opacity:0.8; marker-placement:point;	marker-type:ellipse; marker-allow-overlap:true; } ";
-        //         this.statements[this.topZ] = "sql=" + sql + "&style=" + this.style;
-        //     }
-        // }
     );
 };
