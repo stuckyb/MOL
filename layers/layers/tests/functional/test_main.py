b329ea6bef3030f1d47698d7254e7b9b3b5eb77d
Aaron Steele <eightysteele@gmail.com>
2012-01-25 20:48:43 -0800

    Remove a bunch of old code.

--- layers/layers/tests/functional/test_main.py
+++
@@ -1,7 +0,0 @@
-from layers.tests import *
-
-class TestMainController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='main', action='index'))
-        # Test response...
