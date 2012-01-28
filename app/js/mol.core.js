5b71fbab230e3f5f85e97de6a8b0deca37383f65
Aaron Steele <eightysteele@gmail.com>
2012-01-27 16:11:20 -0800

    Add new mol.core.js module with support for handling layer ids.

---
+++ app/js/mol.core.js
@@ -0,0 +1,31 @@
+/**
+ * This module provides support for rendering search results.
+ */
+mol.modules.core = function(mol) { 
+    
+    mol.core = {};
+    
+    /**
+     * Retunrs a layer id string given a layer name, type, and source.
+     */
+    mol.core.getLayerId = function(name, type, source) {
+        return 'layer-{0}-{1}-{2}'.format(name, type, source);
+    };
+    
+    /**
+     * @param id The layer id of the form "layer-{name}-{type}-{source}".
+     */
+    mol.core.getLayerFromId = function(id) {
+        var tokens = id.split('-'),
+            name = tokens[1],
+            type = tokens[2],
+            source = tokens[3];
+        
+        return {
+            id: id,
+            name: name,
+            type: type,
+            souce: source
+        };
+    };
+};
