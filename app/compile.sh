44e0aaba50945d08e9c293e9d788a6863fc67e1c
Aaron Steele <eightysteele@gmail.com>
2012-01-30 18:09:32 -0800

    Optimize SQL.

--- app/compile.sh
+++ app/compile.sh
@@ -4,6 +4,6 @@ cd js
 
 rm -rf ../static/js/mol.js
 
-cat mol.js mol.bus.js mol.core.js mol.map.js mol.map.layers.js mol.map.menu.js mol.map.results.js mol.map.search.js mol.map.tiles.js mol.mvp.js mol.services.cartodb.js mol.services.js > ../static/js/mol.js
+cat mol.js mol.core.js mol.bus.js mol.mvp.js mol.services.js mol.services.cartodb.js mol.map.js mol.map.layers.js mol.map.menu.js mol.map.results.js mol.map.search.js mol.map.tiles.js > ../static/js/mol.js
 
 cd ..
