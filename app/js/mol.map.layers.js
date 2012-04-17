d9ca863048a0fd5e73964e6b9b79b0b93a365c82
Aaron Steele <eightysteele@gmail.com>
2012-04-17 11:41:33 -0700

    Add feature count to layer widget

--- app/js/mol.map.layers.js
+++ app/js/mol.map.layers.js
@@ -383,7 +383,7 @@ mol.modules.map.layers = function(mol) {
                     '    <button class="source" title="Layer Source: {0}"><img src="/static/maps/search/{0}.png"></button>' +
                     '    <button class="type" title="Layer Type: {1}"><img src="/static/maps/search/{1}.png"></button>' +
                     '    <div class="layerName">' +
-                    '        <div class="layerRecords">{4} records</div>' +
+                    '        <div class="layerRecords">{4} features</div>' +
                     '        <div title="{2}" class="layerNomial">{2}</div>' +
                     '        <div title="{3}" class="layerEnglishName">{3}</div>' +
                     '    </div>' +
@@ -397,7 +397,7 @@ mol.modules.map.layers = function(mol) {
                     '  <div class="break"></div>' +
                     '</div>';
 
-                this._super(html.format(layer.source, layer.type, layer.name, layer.englishname, layer.feature_results));
+                this._super(html.format(layer.source, layer.type, layer.name, layer.englishname, layer.feature_count));
                 this.attr('id', layer.id);
                 this.opacity = $(this).find('.opacity').slider({value: 0.5, min: 0, max:1, step: 0.02, animate:"slow"});
                 this.toggle = $(this).find('.toggle').button();
