3357c960aea8d09679200b44cdb060a9b32cf820
Aaron Steele <eightysteele@gmail.com>
2012-04-18 12:12:27 -0700

    Add threads to name indexer.

--- bulkloader/index_names.py
+++ bulkloader/index_names.py
@@ -6,6 +6,8 @@ import csv_unicode
 import sqlite3
 import re
 
+from Queue import Queue
+import threading
 
 def names():
     print 'Getting names...'
@@ -179,14 +181,60 @@ def setup_cacheitem_db():
     c.close()
     return conn
 
+class Query(object):
+
+    def __init__(self, q, writer):
+        self.q = q
+        self.writer = writer
+
+    def execute(self, name):
+        url = "http://mol.cartodb.com/api/v2/sql?%s" % urllib.urlencode(dict(q="SELECT s.provider as source, p.title as source_title, s.scientificname as name, s.type as type, t.title as type_title, names, n.class as class, m.records as feature_count FROM scientificnames s LEFT JOIN ( SELECT scientific, initcap(lower(array_to_string(array_sort(array_agg(common_names_eng)),', '))) as names, class FROM master_taxonomy GROUP BY scientific, class HAVING scientific = '%s' ) n ON s.scientificname = n.scientific LEFT JOIN (( SELECT count(*) as records, 'points' as type, 'gbif' as provider FROM gbif_import WHERE lower(scientificname)=lower('%s')) UNION ALL (SELECT count(*) as records, type, provider FROM polygons GROUP BY scientificname, type, provider HAVING scientificname='%s' )) m ON s.type = m.type AND s.provider = m.provider LEFT JOIN types t ON s.type = t.type LEFT JOIN providers p ON s.provider = p.provider WHERE s.scientificname = '%s'" % (name, name, name, name)))
+
+        rows = []
+        try:
+            response = urllib2.urlopen(url)
+            if response.code != 200 and response.code != 304: # OK or NOT MODIFIED
+                print 'skipping %s CartoDB response error %s' % (name, response.code)                
+            rows = json.loads(response.read())['rows']
+            print 'CartoDB response received for %s: %s' % (name, rows)
+        except urllib2.HTTPError, e:
+            print 'skipping because of HTTPError code: %s, url: %s' % (e.code, url)
+        except urllib2.URLError, e:
+            print 'skipping because of URLError reason: %s, url: %s ' % (e.reason, url)
+        #except:
+        #    print 'skipping because of unknown cdb error. url: %s' % url
+        
+        self.writer.writerow(dict(id=name.lower(), string=reduce(lambda x,y: '%s,%s' % (x, y), [x for x in rows], [])))
+
+    def loop(self):
+        while True:
+            # Fetch a name from the queue and run the query
+            r = self.q.get()
+            if r == None:
+                self.q.task_done()
+                break
+            else:
+                (name) = r
+            self.execute(name)
+            self.q.task_done()
+
 def create_autocomplete_index():
-    names()
+    #names()
     writer = csv_unicode.UnicodeDictWriter(open('names_index.csv', 'w'), ['id', 'string'])
     writer.writeheader()
     conn = setup_cacheitem_db()
     cur = conn.cursor()
 
     count = 10
+    
+    queue = Queue()
+    renderers = {}
+    num_threads = 100
+    for i in range(num_threads): # number of threads
+        renderer = Query(queue, writer)
+        render_thread = threading.Thread(target=renderer.loop)
+        render_thread.start()
+        renderers[i] = render_thread
 
     for row in csv_unicode.UnicodeDictReader(open('names.csv', 'r')): 
 
@@ -200,31 +248,40 @@ def create_autocomplete_index():
 
         bi = row['binomial_index'] # puma concolor
 
-        # Search result rows for binomial_index:
-        url = "http://mol.cartodb.com/api/v2/sql?%s" % urllib.urlencode(dict(q="SELECT s.provider as source, p.title as source_title, s.scientificname as name, s.type as type, t.title as type_title, names, n.class as class, m.records as feature_count FROM scientificnames s LEFT JOIN ( SELECT scientific, initcap(lower(array_to_string(array_sort(array_agg(common_names_eng)),', '))) as names, class FROM master_taxonomy GROUP BY scientific, class HAVING scientific = '%s' ) n ON s.scientificname = n.scientific LEFT JOIN (( SELECT count(*) as records, 'points' as type, 'gbif' as provider FROM gbif_import WHERE lower(scientificname)=lower('%s')) UNION ALL (SELECT count(*) as records, type, provider FROM polygons GROUP BY scientificname, type, provider HAVING scientificname='%s' )) m ON s.type = m.type AND s.provider = m.provider LEFT JOIN types t ON s.type = t.type LEFT JOIN providers p ON s.provider = p.provider WHERE s.scientificname = '%s'" % (row['binomial'], row['binomial'], row['binomial'], row['binomial'])))
+        queue.put(row['binomial'])
 
-        try:
-            response = urllib2.urlopen(url)
-            if response.code != 200:
-                print 'skipping %s CartoDB response error %s' % (binomial, response.code)
-                continue
-            rows = json.loads(response.read())['rows']
-            print 'CartoDB response received for %s' % binomial
-        except urllib2.HTTPError, e:
-            print 'skipping because of HTTPError code: %s, url: %s' % (e.code, url)
-            continue
-        except urllib2.URLError, e:
-            print 'skipping because of URLError reason: %s, url: %s ' % (e.reason, url)
-            continue
-        except:
-            print 'skipping because of unknown cdb error. url: %s' % url
-            continue
+        # Search result rows for binomial_index:
+        # url = "http://mol.cartodb.com/api/v2/sql?%s" % urllib.urlencode(dict(q="SELECT s.provider as source, p.title as source_title, s.scientificname as name, s.type as type, t.title as type_title, names, n.class as class, m.records as feature_count FROM scientificnames s LEFT JOIN ( SELECT scientific, initcap(lower(array_to_string(array_sort(array_agg(common_names_eng)),', '))) as names, class FROM master_taxonomy GROUP BY scientific, class HAVING scientific = '%s' ) n ON s.scientificname = n.scientific LEFT JOIN (( SELECT count(*) as records, 'points' as type, 'gbif' as provider FROM gbif_import WHERE lower(scientificname)=lower('%s')) UNION ALL (SELECT count(*) as records, type, provider FROM polygons GROUP BY scientificname, type, provider HAVING scientificname='%s' )) m ON s.type = m.type AND s.provider = m.provider LEFT JOIN types t ON s.type = t.type LEFT JOIN providers p ON s.provider = p.provider WHERE s.scientificname = '%s'" % (row['binomial'], row['binomial'], row['binomial'], row['binomial'])))
+
+        # try:
+        #     response = urllib2.urlopen(url)
+        #     if response.code != 200:
+        #         print 'skipping %s CartoDB response error %s' % (binomial, response.code)
+        #         continue
+        #     rows = json.loads(response.read())['rows']
+        #     print 'CartoDB response received for %s' % binomial
+        # except urllib2.HTTPError, e:
+        #     print 'skipping because of HTTPError code: %s, url: %s' % (e.code, url)
+        #     continue
+        # except urllib2.URLError, e:
+        #     print 'skipping because of URLError reason: %s, url: %s ' % (e.reason, url)
+        #     continue
+        # except:
+        #     print 'skipping because of unknown cdb error. url: %s' % url
+        #     continue
         
-        writer.writerow(dict(id=bi, string=reduce(lambda x,y: '%s,%s' % (x, y), [x for x in rows])))
-        if count == 0:
-            return
-        else:
-            count = count - 1
+        # writer.writerow(dict(id=bi, string=reduce(lambda x,y: '%s,%s' % (x, y), [x for x in rows])))
+
+        #if count == 0:
+        #    break
+        #else:
+        #    count = count - 1
+    for i in range(num_threads):
+        queue.put(None)
+    # wait for pending rendering jobs to complete
+    queue.join()
+    for i in range(num_threads):
+        renderers[i].join()
         
 if __name__ == '__main__':
     #names()
