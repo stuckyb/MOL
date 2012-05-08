1888168ac51041a9359f58636b10627c0eef1b1b
Gaurav Vaidya <gaurav@ggvaidya.com>
2012-05-07 18:36:10 -0600

    Added a comment.

--- homepage/appengine_config.py
+++ homepage/appengine_config.py
@@ -1,3 +1,7 @@
+# This was in 'homepage'; I don't know if it still does anything
+# but I'm keeping it around just in case. Delete it if you're use
+# it's unnecessary. -- Gaurav, May 7, 2012.
+
 def webapp_add_wsgi_middleware(app):
     from google.appengine.ext.appstats import recording
     app = recording.appstats_wsgi_middleware(app)
