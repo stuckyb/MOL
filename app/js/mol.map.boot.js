561d4f3684efcbc281d92814f2e367ead83dfa12
Jeremy Malczyk <jmalczyk@gmail.com>
2012-07-20 16:25:06 -0400

    Support for search terms on the URL.

--- app/js/mol.map.boot.js
+++ app/js/mol.map.boot.js
@@ -2,102 +2,104 @@ mol.modules.map.boot = function(mol) {
 
     mol.map.boot = {};
 
-    mol.map.boot.BootEngine = mol.mvp.Engine.extend(
-        {
-            init: function(proxy, bus) {
-                this.proxy = proxy;
-                this.bus = bus;
-                this.IE8 = false;
-                this.sql = '' +
-                    'SELECT * from get_search_results(\'{0}\'); ';
-                this.term = null;
-             },
+    mol.map.boot.BootEngine = mol.mvp.Engine.extend({
+        init: function(proxy, bus) {
+            this.proxy = proxy;
+            this.bus = bus;
+            this.IE8 = false;
+            this.sql = "" +
+                "SELECT DISTINCT l.scientificname as name," +
+                "    l.type as type,"+
+                "    t.title as type_title,"+
+                "    l.provider as source, "+
+                "    p.title as source_title,"+
+                "    n.class as _class, " +
+                "    l.feature_count as feature_count,"+
+                "    n.common_names_eng as names " +
+                "FROM layer_metadata l " +
+                "LEFT JOIN types t ON " +
+                "    l.type = t.type " +
+                "LEFT JOIN providers p ON " +
+                "    l.provider = p.provider " +
+                "LEFT JOIN taxonomy n ON " +
+                "    l.scientificname = n.scientificname " +
+                "WHERE " +
+                "    l.scientificname~*'\\m{0}' OR n.common_names_eng~*'\\m{0}'";
+            this.term = null;
+        },
+        start: function() {
+            this.loadTerm();
+        },
+        /*
+         *   Method to attempt loading layers from search term in the URL.
+         */
+        loadTerm: function() {
+            var self=this;
 
-            /**
-             * Starts the BootEngine. Note that the container parameter is
-             * ignored.
-             */
-            start: function() {
-               this.boot();
-            },
-            boot: function() {
+            // Remove backslashes and characters that should be counted as spaces
+            this.term = unescape(window.location.pathname.replace(/\//g,'').replace(/\+/g,' ').replace(/_/g,' '));
 
-                var self=this;
-
-                this.term = unescape(window.location.pathname.replace(/\//g,'').replace(/\+/g,' ').replace(/_/g,' '));
-
-                if((this.getIEVersion()>=0&&this.getIEVersion()<=8)||this.term=='') {
-                    //If on IE8- or no query params fire the splash event
-                } else {
-                    //Otherwise, try and get a result using term
-                    $.post(
-                        'cache/get',
-                        {
-                            key:'search-results-{0}'.format(self.term),
-                            sql:this.sql.format(self.term)
-                        },
-                        function (response) {
-                            var results = mol.services.cartodb.convert(response);
-                            if(results.length==0) {
-                                self.bus.fireEvent(new mol.bus.Event('toggle-splash'));
-                            } else {
-                                //parse the results
-
-                                self.loadLayers(results.layers);
-                            }
-                        },
-                        'json'
-                     );
+            if((this.getIEVersion()>=0&&this.getIEVersion()<=8)||this.term=='') {
+                // If on IE8- or no query params, fire the splash event
+                self.bus.fireEvent(new mol.bus.Event('toggle-splash'));
+            } else {
+                // Otherwise, try and get a result using term
+                $.post(
+                    'cache/get',
+                    {
+                        key:'search-results-{0}'.format(self.term),
+                        sql:this.sql.format(self.term)
+                    },
+                    function (response) {
+                        var results = mol.services.cartodb.convert(response);
+                        if(Object.keys(results.layers).length==0) {
+                            self.bus.fireEvent(new mol.bus.Event('toggle-splash'));
+                        } else {
+                            //parse the results
+                            self.loadLayers(self.getLayersWithIds(results.layers));
+                        }
+                    },
+                    'json'
+                );
+            }
+        },
+       /*
+        * Adds layers to the map if there are fewer than 25 results, or fires the search results widgetif there are more.
+        */
+        loadLayers: function(layers) {
+            if(Object.keys(layers).length<25) {
+                this.bus.fireEvent(new mol.bus.Event('add-layers',{layers: layers}))
+            } else if (this.term != null) {
+                this.bus.fireEvent(new mol.bus.Event('search',{term: this.term}));
+            }
+        },
+       /*
+        * Returns an array of layer objects {id, name, type, source}
+        * with their id set given an array of layer objects
+        * {name, type, source}.
+        */
+        getLayersWithIds: function(layers) {
+            return  _.map(
+                layers,
+                function(layer) {
+                    return _.extend(layer, {id: mol.core.getLayerId(layer)});
                 }
-            },
-            loadLayers: function(layers) {
-                if(Object.keys(layers).length<100) {
-                    this.bus.fireEvent(new mol.bus.Event('add-layers',{layers: layers}))
-                } else if (this.term != null) {
-                    this.bus.fireEvent(new mol.bus.Event('search',{term: this.term}));
-                }
-            },
-	        /* Returns the version of Internet Explorer or a -1
-            / (indicating the use of another browser).
-            */
-	        getIEVersion: function() {
-  		        var rv = -1, ua,re; // Return value assumes failure.
-  			    if (navigator.appName == 'Microsoft Internet Explorer'){
-    				ua = navigator.userAgent;
-   				    re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-    				if (re.exec(ua) != null){
-      					rv = parseFloat( RegExp.$1 );
-				}
-			}
-  			return rv;
-		},
-		addIframeHandlers: function () {
-		    var self = this;
-
-		    $(this.display.iframe_content[0].contentDocument.body).find('.getspecies').click(
-		          function(event) {
-		               $(self.display).dialog('option','modal','false');
-		               $(self.display.parent()).animate({left: '{0}px'.format($(window).width()/(7/4)-400)}, 'slow');
-		               self.bus.fireEvent(new mol.bus.Event('search', {term:'Puma concolor'}));
-		               setTimeout(function() {self.bus.fireEvent(new mol.bus.Event('results-select-all'))},1000);
-		               setTimeout(function() {self.bus.fireEvent(new mol.bus.Event('results-map-selected'))},2000);
-
-
-		          }
-		    );
-            $(this.display.iframe_content[0].contentDocument.body).find('.listdemo1').click(
-                  function(event) {
-                      $(self.display).dialog('option','modal','false');
-                      $(self.display.parent()).animate({left: '{0}px'.format($(window).width()/3-400)}, 'slow');
-                      self.bus.fireEvent(new mol.bus.Event('layer-display-toggle',{visible: false}));
-                      self.bus.fireEvent(new mol.bus.Event('species-list-query-click', {gmaps_event:{latLng : new google.maps.LatLng(-2.263,39.045)}, map : self.map}));
-                  }
             );
-
-		},
-    }
-    );
+        },
+       /* Returns the version of Internet Explorer or a -1
+        * (indicating the use of another browser).
+        */
+        getIEVersion: function() {
+            var rv = -1, ua, re;
+            // Return value assumes failure.
+            if(navigator.appName == 'Microsoft Internet Explorer') {
+                ua = navigator.userAgent;
+                re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+                if(re.exec(ua) != null) {
+                    rv = parseFloat(RegExp.$1);
+                }
+            }
+            return rv;
+        }
+    });
 };
-
-
-
--- app/js/mol.map.splash.js
+++ app/js/mol.map.splash.js
@@ -2,152 +2,130 @@ mol.modules.map.splash = function(mol) {
 
     mol.map.splash = {};
 
-    mol.map.splash.SplashEngine = mol.mvp.Engine.extend(
-        {
-            init: function(proxy, bus, map) {
-                this.proxy = proxy;
-                this.bus = bus;
-                this.map = map;
-                this.IE8 = false;
-             },
+    mol.map.splash.SplashEngine = mol.mvp.Engine.extend({
+        init : function(proxy, bus, map) {
+            this.proxy = proxy;
+            this.bus = bus;
+            this.map = map;
+            this.IE8 = false;
+        },
+        /**
+         * Starts the MenuEngine. Note that the container parameter is
+         * ignored.
+         */
+        start : function() {
 
-            /**
-             * Starts the MenuEngine. Note that the container parameter is
-             * ignored.
-             */
-            start: function() {
+            this.display = new mol.map.splash.splashDisplay();
+            this.addEventHandlers();
 
-                this.display = new mol.map.splash.splashDisplay();
-                this.addEventHandlers();
-
-            },
-	        // Returns the version of Internet Explorer or a -1
-            // (indicating the use of another browser).
-	        getIEVersion: function() {
-  			   var rv = -1, ua,re; // Return value assumes failure.
-  			   if (navigator.appName == 'Microsoft Internet Explorer'){
-    				ua = navigator.userAgent;
-   				    re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-    				if (re.exec(ua) != null){
-      					rv = parseFloat( RegExp.$1 );
-				    }
-			   }
-  			   return rv;
-		    },
-		    addIframeHandlers: function () {
-		      var self = this;
-
-              $(this.display.iframe_content[0].contentDocument.body).find('.getspecies').click(
-		          function(event) {
-		              $(self.display).dialog('option','modal','false');
-		              $(self.display.parent()).animate({left: '{0}px'.format($(window).width()/(7/4)-400)}, 'slow');
-		              self.bus.fireEvent(new mol.bus.Event('search', {term:'Puma concolor'}));
-		              setTimeout(function() {self.bus.fireEvent(new mol.bus.Event('results-select-all'))},1000);
-		              setTimeout(function() {self.bus.fireEvent(new mol.bus.Event('results-map-selected'))},2000);
-		          }
-		      );
-              $(this.display.iframe_content[0].contentDocument.body).find('.listdemo1').click(
-                  function(event) {
-                      $(self.display).dialog('option','modal','false');
-                      $(self.display.parent()).animate({left: '{0}px'.format($(window).width()/3-400)}, 'slow');
-                      self.bus.fireEvent(new mol.bus.Event('layer-display-toggle',{visible: false}));
-                      self.bus.fireEvent(new mol.bus.Event('species-list-query-click', {gmaps_event:{latLng : new google.maps.LatLng(-2.263,39.045)}, map : self.map}));
-                  }
-               );
-		    },
-		    initDialog: function() {
-                var self = this;
-                this.display.dialog(
-                    {
-                        autoOpen: true,
-                        width: 800,
-                        height: 580,
-                        DialogClass: "mol-splash",
-                        //modal: true
-                    }
-                );
-                $(this.display).width('98%');
-                $(".ui-widget-overlay").live(
-                    "click",
-                    function() {
-                        self.display.dialog("close");
-                    }
-                );
-            },
-            addEventHandlers: function () {
-		      var self = this;
-		      this.bus.addHandler(
-		        'toggle-splash',
-		        function(event) {
-		            if(self.IEVersion()<9 && self.getIEVersion()>=0) {
-                        self.IE8 = true;
-                        //old ie8, please upgrade
-                        self.display.iframe_content.src='/static/splash/ie8.html';
-                        self.initDialog();
-                        self.display.mesg.append($("<div class='IEwarning'>Your version of Internet Explorer requires the Google Chrome Frame Plugin to view the Map of Life. Chrome Frame is available at <a href='http://www.google.com/chromeframe'>http://www.google.com/chromeframe/</a>. Otherwise, please use the latest version of Chrome, Safari, Firefox, or Internet Explorer.</div>"));
-                        $(self.display).dialog( "option", "closeOnEscape", false );
-                        $(self.display).bind(
-                            "dialogbeforeclose",
-                            function(event, ui) {
-                                alert('Your version of Internet Explorer is not supported. Please install Google Chrome Frame, or use the latest version of Chrome, Safari, Firefox, or IE.');
-                                return false;
-                            }
-                        );
-                        $(self.display.iframe_content).height(320);
-                    } else if(self.MOL_Down) {
-                        self.initDialog();
-                        self.display.mesg.append($("<font color='red'>Map of Life is down for maintenance. We will be back up shortly.</font>"));
-                        $(self.display).dialog( "option", "closeOnEscape", false );
-                        $(self.display).bind( "dialogbeforeclose", function(event, ui) {
-                            return false;
-                        });
-                   } else {
-                        self.initDialog();
-                   }
-                   if(!self.IE8) {
-                        $(self.display.iframe_content).load(
-                            function(event) {
-                                self.addIframeHandlers();
-                            }
-                        );
-                   }
+        },
+        // Returns the version of Internet Explorer or a -1
+        // (indicating the use of another browser).
+        getIEVersion : function() {
+            var rv = -1, ua, re;
+            // Return value assumes failure.
+            if(navigator.appName == 'Microsoft Internet Explorer') {
+                ua = navigator.userAgent;
+                re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+                if(re.exec(ua) != null) {
+                    rv = parseFloat(RegExp.$1);
                 }
-             );
-          }
-        }
-    );
-    mol.map.splash.splashDisplay = mol.mvp.View.extend(
-        {
-            init: function() {
-                var html = '' +
-                       '<div class="mol-Splash">' +
-	                   '<div class="message"></div>' +
-	                   '<iframe class="mol-splash iframe_content ui-dialog-content" style="height:400px; width: 98%; margin-left: -18px; margin-right: auto; display: block;" src="/static/splash/index.html"></iframe>' +
-	                   '<div id="footer_imgs" style="text-align: center">' +
-                       '<div>Sponsors, partners and supporters</div>' +
-                       '<a target="_blank" tabindex="-1" href="http://www.yale.edu/jetz/"><button><img width="72px" height="36px" title="Jetz Lab, Yale University" src="/static/home/yale.png"></button></a>' +
-                       '<a target="_blank" tabindex="-1" href="http://sites.google.com/site/robgur/"><button><img width="149px" height="36px" title="Guralnick Lab, University of Colorado Boulder" src="/static/home/cuboulder.png"></button></a>' +
-                       /*'<a target="_blank" href="http://www.iucn.org/"><button><img width="33px" height="32px" title="International Union for Conservation of Nature" src="/static/home/iucn.png"></button></a>' + */
-                       '<a target="_blank" tabindex="-1" href="http://www.gbif.org/"><button><img width="33px" height="32px" title="Global Biodiversity Information Facility" src="/static/home/gbif.png"></button></a>' +
-	                   '<a target="_blank" tabindex="-1" href="http://www.eol.org/"><button><img width="51px" height="32px" title="Encyclopedia of Life" src="http://www.mappinglife.org/static/home/eol.png"></button></a>' +
-	                   '<a target="_blank" tabindex="-1" href="http://www.nasa.gov/"><button><img width="37px" height="32px" title="National Aeronautics and Space Administration" src="http://www.mappinglife.org/static/home/nasa.png"></button></a>' +
-                       '<br>' +
-	                   '<a target="_blank" tabindex="-1" href="http://www.nceas.ucsb.edu/"><button><img width="30px" height="32px" title="National Center for Ecological Analysis and Synthesis" src="http://www.mappinglife.org/static/home/nceas.png"></button></a>' +
-	                   '<a target="_blank" tabindex="-1" href="http://www.iplantcollaborative.org/"><button><img width="105px" height="32px" title="iPlant Collaborative" src="http://www.mappinglife.org/static/home/iplant.png"></button></a>' +
-	                   '<a target="_blank" tabindex="-1" href="http://www.nsf.gov/"><button><img width="32px" height="32px" title="National Science Foundation" src="http://www.mappinglife.org/static/home/nsf.png"></button></a>' +
-	                   '<a target="_blank" tabindex="-1" href="http://www.senckenberg.de"><button><img width="81px" height="32px"title="Senckenberg" src="http://www.mappinglife.org/static/home/senckenberg.png"></button></a>' +
-	                   '<a target="_blank" tabindex="-1" href="http://www.bik-f.de/"><button><img width="74px" height="32px" title="Biodiversität und Klima Forschungszentrum (BiK-F)" src="http://www.mappinglife.org/static/home/bik_bildzeichen.png"></button></a>' +
-	                   '<a target="_blank" tabindex="-1" href="http://www.mountainbiodiversity.org/"><button><img width="59px" height="32px" title="Global Mountain Biodiversity Assessment" src="http://www.mappinglife.org/static/home/gmba.png"></button></a>' +
-	                   '</div>' +
-                       '</div>';
-
-                this._super(html);
-                this.iframe_content = $(this).find('.iframe_content');
-		        this.mesg = $(this).find('.message');
             }
+            return rv;
+        },
+        /*
+         *  Method to attach MOL events to links in the iframe.
+         */
+        addIframeHandlers : function() {
+            var self = this;
+
+            $(this.display.iframe_content[0].contentDocument.body).find('.getspecies').click(function(event) {
+                $(self.display).dialog('option', 'modal', 'false');
+                $(self.display.parent()).animate({
+                    left : '{0}px'.format($(window).width() / (7 / 4) - 400)
+                }, 'slow');
+                self.bus.fireEvent(new mol.bus.Event('search', {
+                    term : 'Puma concolor'
+                }));
+                setTimeout(function() {
+                    self.bus.fireEvent(new mol.bus.Event('results-select-all'))
+                }, 1000);
+                setTimeout(function() {
+                    self.bus.fireEvent(new mol.bus.Event('results-map-selected'))
+                }, 2000);
+            });
+            $(this.display.iframe_content[0].contentDocument.body).find('.listdemo1').click(function(event) {
+                $(self.display).dialog('option', 'modal', 'false');
+                $(self.display.parent()).animate({
+                    left : '{0}px'.format($(window).width() / 3 - 400)
+                }, 'slow');
+                self.bus.fireEvent(new mol.bus.Event('layer-display-toggle', {
+                    visible : false
+                }));
+                self.bus.fireEvent(new mol.bus.Event('species-list-query-click', {
+                    gmaps_event : {
+                        latLng : new google.maps.LatLng(-2.263, 39.045)
+                    },
+                    map : self.map
+                }));
+            });
+        },
+        initDialog : function() {
+            var self = this;
+            this.display.dialog({
+                autoOpen : true,
+                width : 800,
+                height : 580,
+                DialogClass : "mol-splash",
+                //modal: true
+            });
+            $(this.display).width('98%');
+            $(".ui-widget-overlay").live("click", function() {
+                self.display.dialog("close");
+            });
+        },
+        addEventHandlers : function() {
+            var self = this;
+            this.bus.addHandler('toggle-splash', function(event) {
+                if(self.getIEVersion() < 9 && self.getIEVersion() >= 0) {
+                    self.IE8 = true;
+                    //old ie8, please upgrade
+                    self.display.iframe_content.src = '/static/splash/ie8.html';
+                    self.initDialog();
+                    self.display.mesg.append($("<div class='IEwarning'>Your version of Internet Explorer requires the Google Chrome Frame Plugin to view the Map of Life. Chrome Frame is available at <a href='http://www.google.com/chromeframe'>http://www.google.com/chromeframe/</a>. Otherwise, please use the latest version of Chrome, Safari, Firefox, or Internet Explorer.</div>"));
+                    $(self.display).dialog("option", "closeOnEscape", false);
+                    $(self.display).bind("dialogbeforeclose", function(event, ui) {
+                        alert('Your version of Internet Explorer is not supported. Please install Google Chrome Frame, or use the latest version of Chrome, Safari, Firefox, or IE.');
+                        return false;
+                    });
+                    $(self.display.iframe_content).height(320);
+                } else if(self.MOL_Down) {
+                    self.initDialog();
+                    self.display.mesg.append($("<font color='red'>Map of Life is down for maintenance. We will be back up shortly.</font>"));
+                    $(self.display).dialog("option", "closeOnEscape", false);
+                    $(self.display).bind("dialogbeforeclose", function(event, ui) {
+                        return false;
+                    });
+                } else {
+                    self.initDialog();
+                }
+                if(!self.IE8) {
+                    $(self.display.iframe_content).load(function(event) {
+                        self.addIframeHandlers();
+                    });
+                }
+            });
         }
-    );
+    });
+    mol.map.splash.splashDisplay = mol.mvp.View.extend({
+        init : function() {
+            var html = '' + '<div class="mol-Splash">' + '<div class="message"></div>' + '<iframe class="mol-splash iframe_content ui-dialog-content" style="height:400px; width: 98%; margin-left: -18px; margin-right: auto; display: block;" src="/static/splash/index.html"></iframe>' + '<div id="footer_imgs" style="text-align: center">' + '<div>Sponsors, partners and supporters</div>' + '<a target="_blank" tabindex="-1" href="http://www.yale.edu/jetz/"><button><img width="72px" height="36px" title="Jetz Lab, Yale University" src="/static/home/yale.png"></button></a>' + '<a target="_blank" tabindex="-1" href="http://sites.google.com/site/robgur/"><button><img width="149px" height="36px" title="Guralnick Lab, University of Colorado Boulder" src="/static/home/cuboulder.png"></button></a>' +
+            /*'<a target="_blank" href="http://www.iucn.org/"><button><img width="33px" height="32px" title="International Union for Conservation of Nature" src="/static/home/iucn.png"></button></a>' + */
+            '<a target="_blank" tabindex="-1" href="http://www.gbif.org/"><button><img width="33px" height="32px" title="Global Biodiversity Information Facility" src="/static/home/gbif.png"></button></a>' + '<a target="_blank" tabindex="-1" href="http://www.eol.org/"><button><img width="51px" height="32px" title="Encyclopedia of Life" src="http://www.mappinglife.org/static/home/eol.png"></button></a>' + '<a target="_blank" tabindex="-1" href="http://www.nasa.gov/"><button><img width="37px" height="32px" title="National Aeronautics and Space Administration" src="http://www.mappinglife.org/static/home/nasa.png"></button></a>' + '<br>' + '<a target="_blank" tabindex="-1" href="http://www.nceas.ucsb.edu/"><button><img width="30px" height="32px" title="National Center for Ecological Analysis and Synthesis" src="http://www.mappinglife.org/static/home/nceas.png"></button></a>' + '<a target="_blank" tabindex="-1" href="http://www.iplantcollaborative.org/"><button><img width="105px" height="32px" title="iPlant Collaborative" src="http://www.mappinglife.org/static/home/iplant.png"></button></a>' + '<a target="_blank" tabindex="-1" href="http://www.nsf.gov/"><button><img width="32px" height="32px" title="National Science Foundation" src="http://www.mappinglife.org/static/home/nsf.png"></button></a>' + '<a target="_blank" tabindex="-1" href="http://www.senckenberg.de"><button><img width="81px" height="32px"title="Senckenberg" src="http://www.mappinglife.org/static/home/senckenberg.png"></button></a>' + '<a target="_blank" tabindex="-1" href="http://www.bik-f.de/"><button><img width="74px" height="32px" title="Biodiversität und Klima Forschungszentrum (BiK-F)" src="http://www.mappinglife.org/static/home/bik_bildzeichen.png"></button></a>' + '<a target="_blank" tabindex="-1" href="http://www.mountainbiodiversity.org/"><button><img width="59px" height="32px" title="Global Mountain Biodiversity Assessment" src="http://www.mappinglife.org/static/home/gmba.png"></button></a>' + '</div>' + '</div>';
+
+            this._super(html);
+            this.iframe_content = $(this).find('.iframe_content');
+            this.mesg = $(this).find('.message');
+        }
+    });
 };
-
-
-
