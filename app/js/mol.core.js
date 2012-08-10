7f38a9172fea0ebba07ff26751ed0dc80a6e2e17
Jeremy Malczyk <jmalczyk@gmail.com>
2012-08-10 13:01:03 -0400

    Adding extent to layer object.

--- app/js/mol.core.js
+++ app/js/mol.core.js
@@ -18,7 +18,8 @@ mol.modules.core = function(mol) {
             feature_count = $.trim(layer.feature_count).replace(/ /g, "_");
             sourcetype =  $.trim(layer.sourcetype).replace(/ /g, "_");
             _class = $.trim(layer._class).replace(/ /g, "_");
-        return 'layer--{0}--{1}--{2}--{3}--{4}--{5}--{6}--{7}--{8}'.format(name, type, source, names, feature_count, type_title, source_title, sourcetype, _class);
+            extent = $.trim(layer.extent).replace(/ /g, "_").replace(/{/g,'lsq').replace(/}/g,'rsq');
+        return 'layer--{0}--{1}--{2}--{3}--{4}--{5}--{6}--{7}--{8}--{9}'.format(name, type, source, names, feature_count, type_title, source_title, sourcetype, _class, extent);
     };
 
     /**
@@ -35,6 +36,7 @@ mol.modules.core = function(mol) {
             source_title = tokens[7].replace(/_/g, " "),
             sourcetype = tokens[8].replace(/_/g, " ");
             _class = tokens[9].replace(/_/g, " ");
+            extent = tokens[10].replace(/_/g, " ").replace(/lsq/g,'{').replace(/rsq/g,'}');
 
         name = name.charAt(0).toUpperCase()+name.slice(1).toLowerCase();
         source = source.toLowerCase();
@@ -51,7 +53,8 @@ mol.modules.core = function(mol) {
             type_title: type_title,
             source_title: source_title,
             sourcetype: sourcetype,
-            _class : _class
+            _class : _class,
+            extent: extent
         };
     };
 };
