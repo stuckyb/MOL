83f22f0ff637ed4adf056e7ed877345deb5f4aed
jmalczyk <jmalczyk@gmail.com>
2012-09-28 19:44:55 -0400

    Merging cartodb wrapper changes.

--- app/js/mol.core.js
+++ app/js/mol.core.js
@@ -11,9 +11,27 @@ mol.modules.core = function(mol) {
     mol.core.getLayerId = function(layer) {
         var name = $.trim(layer.name.toLowerCase()).replace(/ /g, "_"),
             type = $.trim(layer.type.toLowerCase()).replace(/ /g, "_"),
-            source = $.trim(layer.source.toLowerCase()).replace(/,/g, "").replace(/ /g, "_"),
-            data_table = $.trim(layer.data_table).replace(/,/g, "").replace(/ /g, "_");
-
-        return 'layer--{0}--{1}--{2}--{3}'.format(name, type, source, data_table);
+            source = $.trim(layer.source.toLowerCase()).replace(/,/g, "").replace(/ /g, "_");
+        return 'layer--{0}--{1}--{2}'.format(name, type, source);
     };
-}
+
+    /**
+     * @param id The layer id of the form "layer--{name}--{type}--{source}--{englishname}".
+     */
+    mol.core.getLayerFromId = function(id) {
+        var tokens = id.split('--'),
+            name = tokens[1].replace(/_/g, " "),
+            type = tokens[2].replace(/_/g, " "),
+            source = tokens[3].replace(/_/g, " ");
+        name = name.charAt(0).toUpperCase()+name.slice(1).toLowerCase();
+        source = source.toLowerCase();
+        type = type.toLowerCase();
+
+        return {
+            id: id,
+            name: name,
+            type: type,
+            source: source
+        };
+    };
+};
