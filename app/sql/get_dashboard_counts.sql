d3332df9b4c28a9c03741565f040ebef2019260d
jmalczyk <jmalczyk@gmail.com>
2012-09-28 16:44:51 -0400

    Counter updates

--- app/sql/get_dashboard_counts.sql
+++ app/sql/get_dashboard_counts.sql
@@ -1,7 +1,7 @@
 -- Function to get all MOL layers (checklist and polygon), cache this result in layers_metadata
 DROP function get_dashboard_metadata();
 CREATE FUNCTION get_dashboard_metadata() 
-	RETURNS TABLE(provider text, type text, provider_id text, type_id text, classes text, data_table text, species_count bigint, feature_count bigint) 
+	RETURNS TABLE(provider text, type text, provider_id text, type_id text, classes text, dataset text, species_count bigint, feature_count bigint) 
 AS
 $$
   DECLARE sql TEXT;
@@ -12,14 +12,14 @@ $$
       FOR data in (SELECT * from data_registry) LOOP
 	 SELECT * INTO type FROM type_labels t where t.product_type = data.product_type LIMIT 1;
 	 SELECT * INTO provider FROM providers p where p.provider = data.provider LIMIT 1;
-         IF ((data.type = 'range' or data.type = 'points') and data.provider <> 'gbif')  THEN
+         IF ((data.type = 'range' or data.type = 'points'))  THEN
                 sql = 'SELECT ' || 
                   '   TEXT(''' || provider.title || ''') as provider, ' ||
 	          '   TEXT(''' || type.title || ''') as type, ' ||
 		  '   TEXT(''' || provider.provider || ''') as provider_id, ' ||
 	          '   TEXT(''' || type.product_type || ''') as type_id, ' ||
 		  '   TEXT(''' || data.classes || ''') as classes, ' ||
-                  '   TEXT(''' || data.table_name || ''') as data_table, ' ||
+                  '   TEXT(''' || REPLACE(data.dataset_title,'''','''''') || ''') as dataset, ' ||
                   '   count(DISTINCT '|| data.scientificname || ') as species_count, ' ||
                   '   count(*) as feature_count FROM ' || data.table_name; 
 		IF sql is not Null THEN 
@@ -33,7 +33,7 @@ $$
 		  '   TEXT(''' || provider.provider || ''') as provider_id, ' ||
 	          '   TEXT(''' || type.product_type || ''') as type_id, ' ||
 		  '   TEXT(''' || data.classes || ''') as classes, ' ||
-                  '   TEXT(''' || data.table_name || ''') as data_table, ' ||
+                  '   TEXT(''' || REPLACE(data.dataset_title,'''','''''') || ''') as dataset, ' ||
                   '   count(DISTINCT ' || data.species_id || ') as species_count, ' ||
                   '   count(*) as feature_count ' ||
                   ' FROM ' || data.table_name || ' d';
@@ -48,7 +48,7 @@ $$
 		  '   TEXT(''' || provider.provider || ''') as provider_id, ' ||
 	          '   TEXT(''' || type.product_type || ''') as type_id, ' ||
 		  '   TEXT(''' || data.classes || ''') as classes, ' ||
-                  '   TEXT(''' || data.table_name || ''') as data_table, ' ||
+                  '   TEXT(''' || REPLACE(data.dataset_title,'''','''''') || ''') as dataset, ' ||
                   '   count(DISTINCT ' || data.scientificname || ') as species_count, ' ||
                   '   count(*) as feature_count ' ||
                   ' FROM ' || data.table_name || ' d';
