6284614c67586ab6cf7a088b95c01f95d004dba1
Jeremy Malczyk <jmalczyk@gmail.com>
2012-10-10 15:52:39 -0400

    Switching layer widgets to use source_type for icon instead of source, as in results.

--- app/js/mol.map.layers.js
+++ app/js/mol.map.layers.js
@@ -428,7 +428,7 @@ mol.modules.map.layers = function(mol) {
                     '  <div class="break"></div>' +
                     '</div>';
 
-                this._super(html.format(layer.source, layer.type, layer.name, layer.names, layer.feature_count, layer.source_title, layer.type_title));
+                this._super(html.format(layer.source_type, layer.type, layer.name, layer.names, layer.feature_count, layer.source_title, layer.type_title));
                 this.attr('id', layer.id);
                 this.opacity = $(this).find('.opacity').slider({value: 0.5, min: 0, max:1, step: 0.02, animate:"slow"});
                 this.toggle = $(this).find('.toggle').button();
