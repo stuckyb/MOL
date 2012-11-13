97b20c5b5413b98bb77b309a1f80ef297ae83a46
Jeremy Malczyk <jmalczyk@gmail.com>
2012-11-13 11:04:22 -0500

    Updating sql.

--- app/sql/get_mol_tile.sql
+++ app/sql/get_mol_tile.sql
@@ -17,61 +17,54 @@ CREATE FUNCTION get_mol_tile(text, text, text, text)
 	RETURNS TABLE(cartodb_id text, type text, provider text, seasonality int, the_geom_webmercator geometry) 
 AS
 $$
+
   DECLARE sql TEXT;
   DECLARE data RECORD; -- a data table record
-  DECLARE taxo RECORD; -- a taxonomy table record
-  DECLARE geom RECORD; -- a geometry table record 
   BEGIN
       --assemble some sql to get the tables we want. a a table was passed as a paramater, use that 
       sql = 'SELECT * from data_registry WHERE provider = ''' || $1 || ''' and type = ''' || $2 || '''' ||
-     CASE WHEN $4 <> '' THEN 
-	'  and table_name = ''' || $4 || ''''
+      CASE WHEN $4 <> '' THEN 
+	'  or table_name = ''' || $4 || ''''
 	ELSE ''
-     END;
+      END;
       FOR data in EXECUTE sql LOOP
          IF data.type = 'range' or data.type = 'points' THEN
-                sql = ('SELECT CONCAT('''|| data.table_name ||'-'', cartodb_id) as cartodb_id, TEXT('''||data.type||''') as type, TEXT('''||data.provider||''') as provider, CAST(' || data.seasonality || ' as int) as seasonality, ' || data.geometry_field || ' FROM ' || data.table_name || ' WHERE ' ||  data.scientificname || ' = ''' || $3 || '''');
-                 RETURN QUERY EXECUTE sql;
-         ELSIF data.type = 'checklist' and data.taxo_table <> Null and data.geom_table <> Null THEN 		
-		-- Get the sciname and species_id field names from the checklist taxonomy table
-	        sql = 'SELECT d.scientificname, d.species_id INTO taxo FROM data_registry d WHERE d.table_name = ''' || data.taxo_table  || ''' LIMIT 1';
-	        EXECUTE sql;
-		-- Get the geom_id field from the checklist geometry table
-		sql = 'SELECT d.geom_id INTO geom FROM data_registry d WHERE table_name = ''' || data.geom_table || ''' LIMIT 1';
-	        EXECUTE sql;
-                -- Glue them all together
-		sql = ' SELECT ' ||
-	          ' d.cartodb_id as data_id, ' ||   
-                  ' g.' || geom.geom_id || ' as geom_id, ' || 
-                  ' t.' || taxo.species_id || ' as species_id, ' ||
-                  ' g.the_geom_webmercator as the_geom_webmercator ' ||   
+                sql := 'SELECT ' ||
+		  ' CONCAT('''|| data.table_name ||'-'', cartodb_id) as cartodb_id, ' ||
+                  ' TEXT('''||data.product_type||''') as type, TEXT('''||data.provider||''') as provider, ' ||
+                  ' CAST(' || data.seasonality || ' as int) as seasonality, ' || 
+                  data.geometry_field || 
+                  ' FROM ' || data.table_name || 
+                  ' WHERE ' ||  
+                  data.scientificname || ' = ''' || $3 || '''';               
+         ELSIF data.type = 'taxogeochecklist' or data.type = 'taxogeooccchecklist' THEN 		
+                sql := 'SELECT ' ||
+		  ' DISTINCT CONCAT('''|| data.table_name || '-'', d.cartodb_id) as cartodb_id, ' ||
+                  ' TEXT('''||data.product_type||''') as type, TEXT('''||data.provider||''') as provider, ' ||
+                  ' CAST(' || data.seasonality || ' as int) as seasonality, ' || 
+                  ' g.' || data.geometry_field || 
                   ' FROM ' || data.table_name || ' d ' ||
+	          ' JOIN ' || data.geom_table || ' g ON ' ||
+                  '   d.' || data.geom_id || ' = g.' || data.geom_link_id  ||
                   ' JOIN ' || data.taxo_table || ' t ON ' ||
-                  '   d.' || data.species_id || ' = t.' || taxo.species_id ||
-                  ' JOIN ' || data.geom_table || ' g ON ' ||
-                  '   d.' || data.geom_id || ' = g.' || geom.geom_id  ||
-		  ' WHERE t.' || taxo.scientificname || ' = $3'; 
-		RETURN QUERY EXECUTE sql;
-	  ELSIF data.type = 'checklist' and data.taxo_table = Null and data.scientificname <> Null THEN 
-		-- Get the geom_id field from the checklist geometry table
-		sql = 'SELECT d.geom_id INTO geom FROM data_registry d WHERE table_name = ''' || data.geom_table || ''' LIMIT 1';
-	        EXECUTE sql;
-                -- Glue them all together
-		sql = ' SELECT ' || 
-                  '   d.' || taxo.scientificname || ', ' ||
-                  '   TEXT(''' || data.type || ''') as type, ' || 
-                  '   TEXT(''' || data.provider || ''') as provider, ' ||
-                  '   TEXT(''' || data.table_name || ''') as data_table, ' ||
-                  '   ST_Extent(g.the_geom) as extent, ' ||
-                  '   count(*) as feature_count ' ||
+                  '   d.' || data.species_id || ' = t.' || data.species_link_id ||
+		  ' WHERE ' || data.scientificname || ' = ''' || $3 || ''''; 
+	  ELSIF data.type = 'geochecklist' THEN 
+		sql := 'SELECT ' ||
+		  ' DISTINCT CONCAT('''|| data.table_name ||'-'', d.cartodb_id) as cartodb_id, ' ||
+                  ' TEXT('''||data.product_type||''') as type, TEXT('''||data.provider||''') as provider, ' ||
+                  ' CAST(' || data.seasonality || ' as int) as seasonality, ' || 
+                  ' g.' || data.geometry_field || 
                   ' FROM ' || data.table_name || ' d ' ||
                   ' JOIN ' || data.geom_table || ' g ON ' ||
-                  '   d.' || data.geom_id || ' = g.' || geom.geom_id  ||
-		  ' where d.' || data.scientificname || ' = $3 ';
-		RETURN QUERY EXECUTE sql;
+                  '   d.' || data.geom_id || ' = g.' || data.geom_link_id  ||
+		  ' where ' || data.scientificname || ' = ''' ||  $3 || '''';
            ELSE
                 -- We got nuttin'
 	  END IF;
+	  --RETURN sql;
+          RETURN QUERY EXECUTE sql;
        END LOOP;
     END
+
 $$  language plpgsql;
