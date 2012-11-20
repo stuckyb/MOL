3a36daca62c13de28ff2f9d58b06310f9185c816
Jeremy Malczyk <jmalczyk@gmail.com>
2012-11-20 15:42:46 -0500

    Playing with tile handler.

--- app/ee_handler.py
+++ app/ee_handler.py
@@ -9,6 +9,7 @@ from oauth2client.appengine import AppAssertionCredentials
 
 #Global variables
 EE_URL = 'https://earthengine.googleapis.com'
+CDB_URL = 'http://mol.cartodb.com/api/v2/sql'
 
 # The OAuth scope URL for the Google Earth Engine API.
 GEE_SCOPE = 'https://www.googleapis.com/auth/earthengine.readonly'
@@ -24,9 +25,15 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         
         ee.Initialize(credentials, EE_URL)
-        mapid = ee.Image('srtm90_v4').getMapId({'min':0, 'max':1000})
-
-
+        sciname = self.request.get('sciname', None)
+        cover = ee.Image('MCD12Q1/MCD12Q1_005_2001_01_01').select('Land_Cover_Type_1');
+        elev = ee.Image('srtm90_v4');
+        output = ee.Image(0)
+        species = ee.FeatureCollection('ft:1ugWA45wi7yRdIxKAEbcfd1ks8nhuTcIUyx1Lv18').filter(ee.Filter().eq('Latin',sciname))
+        Image1 = ee.Image(0).mask(0)
+        Image1.paint(species, 0,3)
+        mapid = Image1.getMapId({'min':0, 'max':1000})
+        
         template_values = {
           'mapid' : mapid['mapid'],
           'token' : mapid['token']
