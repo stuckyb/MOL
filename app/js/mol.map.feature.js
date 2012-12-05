0ac96e92c04722dd1cf0b906c51d08515b74d8c5
Tom Auer <mta138@gmail.com>
2012-12-05 14:27:50 -0500

    Continued feature clicking work.

--- app/js/mol.map.feature.js
+++ app/js/mol.map.feature.js
@@ -8,9 +8,9 @@ mol.modules.map.feature = function(mol) {
             this.bus = bus;
             this.map = map;
             //TODO add
-            this.url = '';
+            this.url = 'http://mol.cartodb.com/api/v2/sql?callback=?&q={0}';
             //TODO add
-            this.sql = '';
+            this.sql = "SELECT * FROM get_features('{0}',{1},'{2}')";
             
             this.clickDisabled = false;
         },
@@ -34,19 +34,85 @@ mol.modules.map.feature = function(mol) {
             google.maps.event.addListener(
                 self.map,
                 "click",
-                function () {
-                    //TODO also check if layers are visible
-                    //check this from results?
+                function (mouseevent) {
+                    var reqLays = [],
+                        tolerance = 5,
+                        sql;
+                        
                     if(!self.clickDisabled && 
                         self.map.overlayMapTypes.length > 0) {
-                        //TODO make request here
+                        
+                        //latlng
+                        mouseevent.latLng;
+                        
+                        //pixels
+                        tolerance;
+                        
+                        //layer id list
+                        self.map.overlayMapTypes.forEach(
+                            function(mt) {
+                                if(mt.opacity != 0) {
+                                    reqLays.push(mt.name);
+                                }
+                            }  
+                        );       
+
+                        
+                        sql = self.sql.format(
+                                mouseevent.latLng.toString(),
+                                tolerance,
+                                reqLays.toString()
+                        );
+                        
+                        console.log(self.map.overlayMapTypes);   
+                        
+                        
+                        //make request
+                        $.getJSON(
+                            self.url.format(sql),
+                            function(data, textStatus, jqXHR) {
+                                var results = {
+                                        latlng: mouseevent.latLng,
+                                        response: data
+                                    },
+                                    e = new mol.bus.Event(
+                                        'feature-results',
+                                        results
+                                    );
+                                    
+                                //call process results    
+                                    
+                                self.bus.fireEvent(e);
+                            }
+                        );
+                        
                     }
+                }
+            );
+            
+            this.bus.addHandler(
+                'feature-results',
+                function(event) {
+                    self.showFeatures(event.params);
                 }
             );
         },
         
         addFeatureDisplay : function() {
             this.display = new mol.map.FeatureDisplay();
+        },
+        
+        showFeatures: function(params) {
+            var infowindow = new google.maps.InfoWindow();
+            infowindow.setPosition(params.latlng);
+            
+            console.log("showFeatures");
+            //$('.mol-Map-FeatureDisplay')
+            
+            
+            //infowindow.setContent();
+            //infowindow.open(self.map);
+            
         }
     });
     
