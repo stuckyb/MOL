b49802faf9d6b134890666cee7c5a22a64d9784b
Tom Auer <mta138@gmail.com>
2012-12-05 09:00:30 -0500

    Feature model skeleton.

--- app/js/mol.map.feature.js
+++ app/js/mol.map.feature.js
@@ -11,14 +11,53 @@ mol.modules.map.feature = function(mol) {
             this.url = '';
             //TODO add
             this.sql = '';
+            
+            this.clickDisabled = false;
         },
 
         start : function() {
+            this.addFeatureDisplay();
             this.addEventHandlers();
         },
         
         addEventHandlers : function () {
             var self = this;
+            
+            this.bus.addHandler(
+                'layer-click-toggle',
+                function(event) {
+                    self.clickDisabled = event.disable;
+                }
+            );
+                
+            //may want to wait to add this until ready
+            google.maps.event.addListener(
+                self.map,
+                "click",
+                function () {
+                    //TODO also check if layers are visible
+                    //check this from results?
+                    if(!self.clickDisabled && 
+                        self.map.overlayMapTypes.length > 0) {
+                        //TODO make request here
+                    }
+                }
+            );
+        },
+        
+        addFeatureDisplay : function() {
+            this.display = new mol.map.FeatureDisplay();
+        }
+    });
+    
+    mol.map.FeatureDisplay = mol.mvp.View.extend({
+        init : function(names) {
+            var className = 'mol-Map-FeatureDisplay',
+                html = '' +
+                    '<div></div>';
+
+            this._super(html);
         }
     });
 }
+
