467fa29a1644328814864b7a5a94818b786f9f13
Jeremy Malczyk <jmalczyk@gmail.com>
2013-01-04 16:37:10 -0500

    Layer union and simplification.

--- app/js/mol.map.editor.js
+++ app/js/mol.map.editor.js
@@ -11,6 +11,7 @@ mol.modules.map.editor = function(mol) {
         },
 
         start : function() {
+            this.addEditorDisplay();
             this.addEventHandlers();
             //disable all map clicks
             this.toggleMapLayerClicks(false);
@@ -21,18 +22,28 @@ mol.modules.map.editor = function(mol) {
             this.bus.fireEvent(
                 new mol.bus.Event('layer-click-toggle', {disable: boo}));          
         },
+        addEditorDisplay : function() {
+            var params = {
+                display: null,
+                slot: mol.map.ControlDisplay.Slot.TOP,
+                position: google.maps.ControlPosition.TOP_RIGHT
+            };
+            
+            this.display = new mol.map.editor.EditorControlDisplay();
+            params.display = this.display;
+            this.bus.fireEvent(new mol.bus.Event('add-map-control', params));
+        },
         addEditableLayer : function (json) {
             var self = this;
             this.layer = [];
-            if(this.editer = undefined) {
+            if(this.editer == undefined) {
                 this.editor = new google.maps.drawing.DrawingManager({
-                      drawingMode: google.maps.drawing.OverlayType.MARKER,
+                      drawingMode: google.maps.drawing.OverlayType.POLYGON,
                       drawingControl: true,
                       drawingControlOptions: {
                         position: google.maps.ControlPosition.TOP_CENTER,
                         drawingModes: [
-                          google.maps.drawing.OverlayType.POLYGON,
-                          google.maps.drawing.OverlayType.MARKER
+                          google.maps.drawing.OverlayType.POLYGON
                         ]
                       },
                       polygonOptions: {
@@ -54,7 +65,7 @@ mol.modules.map.editor = function(mol) {
                         feature = new GeoJSON(geojson);
                     if(feature.setMap != undefined) {
                         //feature.draggable=true;
-                        feature.editable=true;
+                        feature.setOptions({editable:true});
                         feature.setMap(self.map)
                     } else {
                     
@@ -63,7 +74,7 @@ mol.modules.map.editor = function(mol) {
                             function(f) {
                                 if(f.setMap != undefined) {
                                     //f.draggable=true;
-                                    f.editable=true;
+                                    f.setOptions({editable:true});
                                     f.setMap(self.map);
                                 }
                                 self.layer.push(f);
@@ -75,21 +86,104 @@ mol.modules.map.editor = function(mol) {
             
             
         },
+        defineRange: function () {
+            var name = prompt(
+                'This feature creates a new range map based on currently ' +
+                'visible layers. What species (scientific name) would you like ' +
+                'to define a range for?'
+            );
+            if (name) {
+                this.startEditing(name);
+            }
+        },
+        startEditing: function(name) {
+            //TODO: zoom to max layer extent
+            //first get a very simplified convex hull of all available maps
+            var layers = [], 
+                tiles = [],
+                gridres = 40075000/(256^this.map.getZoom()), 
+                tilesql = ''+
+                    'SELECT {0} as geom '+
+                    'FROM get_tile(\'{1}\',\'{2}\',\'{3}\',\'{4}\')';
+                sql = '' +
+                    'SELECT ST_AsGeoJson(' +
+                            'ST_Transform(' +
+                                'ST_SimplifyPreserveTopology(' +
+                                    'ST_Union(' +
+                                        'ST_Buffer(g.geom,0)' +
+                                    '),' +
+                                    '150000' +
+                                '),' +
+                                '4326' +
+                            ')' +
+                        ') as geom ' +
+                    'FROM ({0}) g ';
+                    
+            //make an array of layer ids
+            this.map.overlayMapTypes.forEach(
+                function(mt,i) {
+                    if(mt.name.split('--')[2]!='points') {
+                        layers.push(mt.name);
+                    }
+                }
+            )
+            tiles = _.map(
+                layers,
+                function(layer) {
+                    var collectsql = (layer.split('--')[2] == 'points') ?
+                        'ST_SnapToGrid(ST_ConcaveHull(' +
+                            'ST_Collect('+
+                                'the_geom_webmercator'+
+                            '),' +
+                            '0.90' +
+                        '),50000)' :
+                        ' ST_SnapToGrid(the_geom_webmercator,10000) ',
+                        source = layer.split('--')[3], //source
+                        type = layer.split('--')[2], //type
+                        name = layer.split('--')[1],
+                        dataset_id = layer.split('--')[4];
+                    name = '{0}{1}'
+                        .format(name[0].toUpperCase(),name.substr(1))
+                        .replace('_', ' ');
+                            
+                    return tilesql.format(
+                            collectsql,
+                            source,
+                            type,
+                            name,
+                            dataset_id
+                    );
+                }
+            );
+            
+            $.getJSON(
+                mol.services.cartodb.sqlApi.jsonp_url.format(
+                    sql.format(
+                        tiles.join(' UNION ')
+                    )
+                ),
+                this.addEditableLayer.bind(this)
+            );
+        },
         addEventHandlers : function () {
             var self = this;
             /*
-             *  Makes a layer editable by removing it from teh map and adding it
+             *  Makes a layer editable by removing it from the map and adding it
              *  back as an editable polygon, simplified to match the zoom level
              */
+            this.display.defineRange.click(
+                  function(event) {
+                      self.defineRange();
+                  }
+            );
             this.bus.addHandler(
                 'edit-layer',
                 function(event) {
                     var layer = event.layer,
                         gridres = 40075000/(256^self.map.getZoom()), 
                         sql = 'SELECT ' +
-                            'ST_AsGeoJson(ST_Transform(' +
-                                'ST_Simplify(ST_SnapToGrid(the_geom_webmercator,1000),10000)' +
-                                ',4326)' +
+                            'ST_AsGeoJson('+
+                                'ST_Transform(the_geom_webmercator,4326)' +
                             ') as geom ' +
                             'FROM get_tile(\'{1}\',\'{2}\',\'{3}\',\'{4}\')';
                         url = mol.services.cartodb.sqlApi.jsonp_url.format(
@@ -101,7 +195,14 @@ mol.modules.map.editor = function(mol) {
                                 layer.dataset_id
                             )
                         )
-                        
+                    self.map.overlayMapTypes.forEach(
+                        function(mt,i) {
+                            if(mt.name==layer.id) {
+                                self.maptype = self.map.overlayMapTypes.removeAt(i);
+                                self.maptype_index = i;
+                            }
+                        }
+                    )
                     $.getJSON(
                         url,
                         self.addEditableLayer.bind(self)
@@ -111,16 +212,30 @@ mol.modules.map.editor = function(mol) {
         },
     });
 
-    mol.map.EditorDisplay = mol.mvp.View.extend({
+    mol.map.editor.EditorControlDisplay = mol.mvp.View.extend({
         init : function(names) {
-            var className = 'mol-Map-EditorDisplay',
-                html = '' +
-                    '<button class="edit">' +
-                        'Edit' 
-                    '</button>';
+            var html = '' +
+                    '<div class="mol-Map-EditorDisplay widgetTheme">' +
+                        '<button class="edit">' +
+                            'Define Range' + 
+                        '</button>' +
+                    '</div>';
 
             this._super(html);
-            this.editButton=$(this).find('.edit');
+            
+            this.defineRange=$(this).find('.edit');
+        }
+    });
+    mol.map.editor.NewRangeDialog = mol.mvp.View.extend({
+            init: function() {
+                var html = '' +
+                    '<div id="dialog">' +
+                        'This feature creates a new range map based on ' +
+                        'layers visible on the map.' +
+                        '<br>' +
+                    '</div>  ';
+                this._super(html);
+
         }
     });
 };
